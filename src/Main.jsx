359c8ce08a4b40efc2dcdb926491157f8ced8b09
fdavison1 <fred.noreply@gmail.com>
2020-12-23 12:33:48 -0600

    import svg

---
+++ src/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import thread from './assets/thread.svg';
+
+export const Main = () => {
+    return (
+        <div className='flex'>
+            <img src={thread} alt='ball of thread'/>
+        </div>
+    )
+}
